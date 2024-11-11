f2c91f8569f63059ce103aab5e06461c27315a66
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-11-11 07:22:24 +0530

    sdvsd

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Increase the request body size limit
+app.use(express.json({ limit: "10mb" })); // Adjust the size as needed
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 // Middleware
 app.use(bodyParser.json());
 app.use(express.json());
